05191e1019f9be45e1216a01af6513855f325ab5
TimonSchreiber <timon.schreiber@live.de>
2025-01-15 18:15:38 +0100

    Fixes formatting.

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
-import { LandingPageComponent } from './pages/landing-page/landing-page.component';
-import { AuthGuardService } from './services/auth-guard.service';
+import { RouterModule, Routes } from '@angular/router';
+import { AddUserPageComponent } from './pages/add-user-page/add-user-page.component';
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 import { ExamplePageComponent } from './pages/example-page/example-page.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
-import {DashboardPageComponent} from './pages/dashboard-page/dashboard-page.component';
-import {SalesmanDetailsPageComponent} from './pages/salesman-details-page/salesman-details-page.component';
-import {AddUserPageComponent} from './pages/add-user-page/add-user-page.component';
+import { SalesmanDetailsPageComponent } from './pages/salesman-details-page/salesman-details-page.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 /*
   This array holds the relation of paths and components which angular router should resolve.
@@ -19,13 +19,13 @@ import {AddUserPageComponent} from './pages/add-user-page/add-user-page.componen
   Look at: frontend/src/app/components/menu-bar/menu-bar.component.ts
  */
 const routes: Routes = [
-    {path: 'login', component: LoginPageComponent},
-    {path: 'example', component: ExamplePageComponent, canActivate: [AuthGuardService]},
-    {path: 'dashboard', component: DashboardPageComponent, canActivate: [AuthGuardService]},
-    {path: 'add-user', component: AddUserPageComponent, canActivate: [AuthGuardService]},
-    {path: 'salesmen/:id', component: SalesmanDetailsPageComponent, canActivate: [AuthGuardService]},
-    {path: '', component: LandingPageComponent, canActivate: [AuthGuardService]},
-    {path: '**', component: NotFoundPageComponent} // these entries are matched from top to bottom => not found should be the last entry
+    { path: 'login', component: LoginPageComponent },
+    { path: 'example', component: ExamplePageComponent, canActivate: [AuthGuardService] },
+    { path: 'dashboard', component: DashboardPageComponent, canActivate: [AuthGuardService] },
+    { path: 'add-user', component: AddUserPageComponent, canActivate: [AuthGuardService] },
+    { path: 'salesmen/:id', component: SalesmanDetailsPageComponent, canActivate: [AuthGuardService] },
+    { path: '', component: LandingPageComponent, canActivate: [AuthGuardService] },
+    { path: '**', component: NotFoundPageComponent } // these entries are matched from top to bottom => not found should be the last entry
 ];
 
 @NgModule({
