02cd50f5121c19399d6ccbba1de1475bdd82b03e
kmai <khaiminhmai2@gmail.com>
2025-01-17 12:54:28 +0100

    frontend: menubar & some css minor fixes

--- frontend/src/app/components/menu-bar/menu-bar.component.ts
+++ frontend/src/app/components/menu-bar/menu-bar.component.ts
@@ -20,6 +20,7 @@ export class MenuBarComponent implements OnInit {
         {title: 'Welcome', routerLink: ''}, // the tile is the text on the button, the routerLink specifies, where it will navigate
         {title: 'Example', routerLink: 'example'},
         {title: 'Dashboard', routerLink: 'dashboard'},
+        {title: 'Verify Bonuses', routerLink: 'verify-bonus-salaries'},
         {title: 'Add User', routerLink: 'add-user'},
     ];
 
